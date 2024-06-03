fb43e07625e3d077b9008f8b89f48403cd5c8389
Gyanendra <gyanen56@gmail.com>
2024-06-03 12:22:16 +1000

    Table Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import UserForm from './components/UserForm';
+import UserTable from './components/UserTable';
 
 const  App=()=> {
   return (
    <div className='container'>
+    <h4 className='pt-4 text-center'>User List</h4>
+    <hr />
     <UserForm/>
+    <UserTable/>
    </div>
   );
 }
--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,17 +1,17 @@
 const UserForm = () => {
    
   return (
-    <div className="w-50 m-auto p-5 shadow-lg mt-5">
+    <div className="w-50 m-auto p-5 shadow-lg mt-5 rounded-3 mb-5">
       <form action="" >
-        <div className="row">
-          <div className="col-3">
+        <div className="row g-2">
+          <div className="col-md-3">
             <select id="inputState" className="form-select" name="gender" required>
               <option >Gender</option>
               <option value="m">Male</option>
               <option value="f">Female</option>
             </select>
           </div>
-          <div className="col-6">
+          <div className="col-md-6">
             <input
               name="name"
               type="text"
@@ -22,7 +22,7 @@ const UserForm = () => {
          
             />
           </div>
-          <button className="col-3 btn btn-primary d-grid">Add User</button>
+          <button className="col-md-3 btn btn-primary d-grid">Add User</button>
         </div>
       </form>
     </div>
